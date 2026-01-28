efe87a037d6205888478b2383fbf95757a1d246c
Pablogabi07 <pablojuares8793@gmail.com>
2026-01-28 13:41:39 -0300

    Se modifica check-stock

--- api/check-stock.js
+++ api/check-stock.js
@@ -1,12 +1,14 @@
-export const config = { runtime: "edge" }
+export const config = {
+  runtime: "edge",
+}
 
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2"
+import { createClient } from "@supabase/supabase-js"
 
 export default async function handler(req) {
   try {
     const supabase = createClient(
-      Deno.env.get("SUPABASE_URL"),
-      Deno.env.get("SUPABASE_KEY")
+      process.env.SUPABASE_URL,
+      process.env.SUPABASE_KEY
     )
 
     const { cart } = await req.json()
